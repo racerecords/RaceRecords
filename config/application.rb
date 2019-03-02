5f414efa00abbe2ef91f263945b2481f05b9251c
mcmillanator <me@stanleymcmillan.com>
2019-03-02 18:16:04 -0500

    add app env variable for development server settings

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RaceRecords
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.env = ENV['APP_ENV'] || 'production'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
